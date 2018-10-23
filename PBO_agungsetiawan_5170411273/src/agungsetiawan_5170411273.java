e45cd9b3e522ec80b11e65d3bfe987293d974722
setiawanagung1 <44408341+setiawanagung1@users.noreply.github.com>
2018-10-23 22:46:11 +0700

    Add files via upload

---
+++ PBO_agungsetiawan_5170411273/src/agungsetiawan_5170411273.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class agungsetiawan_5170411273 {
+    public static void main(String[]args){
+
+int pil;
+Scanner s = new Scanner (System.in);
+
+for (pil=1; pil<=3;pil++)
+{
+    System.out.println("1. Input data ");
+    System.out.println("2. Tampil data ");
+    System.out.println("3. Keluar ");
+    System.out.print("Pilihan: "); pil = s.nextInt();
+}
+        
+}
+}
